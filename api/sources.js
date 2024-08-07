f1060b64d74fb39f14b37853527c1b4b1974e469
smallT123456 <shaffinmalik9@gmail.com>
2024-08-07 16:17:19 +0500

    Commit # 3

--- api/sources.js
+++ api/sources.js
@@ -44,7 +44,7 @@ async function addSources(db, req, res, workspaceId) {
         } catch (err) {
             console.error('Failed to add source:', err);
             res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Failed to add source' }));
+            res.end(JSON.stringify({ error: err }));
         }
     });
 }
